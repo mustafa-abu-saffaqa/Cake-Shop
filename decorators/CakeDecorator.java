ca96b3535dd30a57eb4bd20b7354f61e552f452f
Amer-Abuyaqob <a.abwycwb@gmail.com>
2025-12-13 17:03:20 +0200

    Added CakeDecorator class implementation

---
+++ decorators/CakeDecorator.java
@@ -0,0 +1,54 @@
+package decorators;
+
+import cake.Cake;
+
+/**
+ * Abstract base class for cake decorators following the Decorator pattern.
+ * 
+ * <p>This class wraps a {@link Cake} instance and delegates method calls to it,
+ * allowing concrete decorators to enhance the cake's description and cost without
+ * modifying the original cake object.
+ * 
+ * <p>Concrete decorators (e.g., ChocolateChips, Cream, Skittles) extend this
+ * class and override {@link #describe()} and {@link #getCost()} to add their
+ * specific features and costs.
+ * 
+ * @author Amer Abuyaqob
+ * @version 1.0
+ */
+public abstract class CakeDecorator extends Cake {
+    protected Cake decoratedCake;
+
+    /**
+     * Constructs a new CakeDecorator wrapping the given cake.
+     * 
+     * @param decoratedCake The cake instance to be decorated
+     */
+    public CakeDecorator(Cake decoratedCake) {
+        super(decoratedCake.getOrderID(), decoratedCake.getBaseName(), 
+              decoratedCake.getSize(), decoratedCake.getBasePrice());
+        this.decoratedCake = decoratedCake;
+    }
+
+    /**
+     * Returns the description of the decorated cake.
+     * Concrete decorators should override this to append their own description.
+     * 
+     * @return A description of the cake with all decorations
+     */
+    @Override
+    public String describe() {
+        return this.decoratedCake.describe();
+    }
+
+    /**
+     * Returns the total cost of the decorated cake.
+     * Concrete decorators should override this to add their own cost.
+     * 
+     * @return The total cost including all decorations
+     */
+    @Override
+    public double getCost() {
+        return this.decoratedCake.getCost();
+    }
+}
